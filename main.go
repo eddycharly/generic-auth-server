bb2b8e393fd7cdd4bb1b9bd3994794dc887dafb1
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-11-20 23:13:21 +0100

    fix: improve logger config
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"os"
 
 	"github.com/eddycharly/generic-auth-server/pkg/commands/root"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 func main() {
+	log.SetLogger(zap.New())
 	root := root.Command()
 	if err := root.Execute(); err != nil {
 		os.Exit(1)
--- pkg/commands/serve/command.go
+++ pkg/commands/serve/command.go
@@ -15,8 +15,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/clientcmd"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 func Command() *cobra.Command {
@@ -31,7 +29,6 @@ func Command() *cobra.Command {
 		Use:   "serve",
 		Short: "Start the authz server",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			log.SetLogger(zap.New())
 			// setup signals aware context
 			return signals.Do(context.Background(), func(ctx context.Context) error {
 				// track errors
